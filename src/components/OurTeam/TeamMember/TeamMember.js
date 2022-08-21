ef27af97e2441b4a5337c2c7119422903eb5a913
RinaOcean <70663656+RinaOcean@users.noreply.github.com>
2022-08-21 17:31:17 +0200

    cards animation

--- src/components/OurTeam/TeamMember/TeamMember.js
+++ src/components/OurTeam/TeamMember/TeamMember.js
@@ -1,28 +1,45 @@
-// import {useState} from 'react'
+import { useRef, useState } from 'react';
+import { IconButton } from '@mui/material';
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import RemoveCircleRoundedIcon from "@mui/icons-material/RemoveCircleRounded";
 import styles from './TeamMember.module.css';
+import { useEffect } from 'react';
 
 const TeamMember = (props) => {
-    const {img, name, job_title, bio} = props.member
+    const { img, name, job_title, bio } = props.member
+    const [open, setOpen] = useState(false) 
+    
     return (
-        
-            <div className={styles.theCard}>
-                <div className={styles.theFront}>
-                    <div className={styles.imgWrapper}>
+        <div className={styles.theCard}>
+            <div className={styles.theFront}>
+                <div className={styles.imgWrapper}>
                     <img src={img} className={styles.img} alt={img} />
-                    <div className={styles.label}>
-                    <h1 className={styles.name}>{name}</h1>
-                    <h2 className={styles.jobTitle}>{job_title}</h2>
-                    </div>
-                    </div>
                     
-                </div>
-                <div className={styles.theBack}>
-                    {/* <h1 className={styles.name}>{name}</h1>
-                    <h2 className={styles.jobTitle}>{job_title}</h2> */}
-                    <span className={styles.bio}>{bio}</span>
+                        <div className={open ? styles.theBackOpen : styles.theBackClosed }>
+                            <span className={styles.bio}>{bio}</span>
+                        </div>
+                    
+                    <div className={styles.label}>
+                        <button
+                            className={styles.expandBtn}
+                            type="button"
+                            onClick={() => {
+                                setOpen(!open) 
+                            } }
+                        >
+                            {open ? (
+                                <RemoveCircleRoundedIcon fontSize="medium" />
+                            ) : (
+                                <AddCircleRoundedIcon fontSize="medium" />
+                            )}
+                        </button>
+                        <h1 className={styles.name}>{name}</h1>
+                        <h2 className={styles.jobTitle}>{job_title}</h2>
+                    </div>
                 </div>
             </div>
-          )
+        </div>
+    );
 }
 
 export default TeamMember;

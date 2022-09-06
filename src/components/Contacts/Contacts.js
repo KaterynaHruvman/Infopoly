9b117975120ab0dea8f564a00a645c83427b1c00
Ekateryna Gruvman <kate.gruvman@ukr.net>
2022-09-06 10:30:43 +0300

    fixed contacts page

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,43 +5,44 @@ import { Link, NavLink } from 'react-router-dom';
 import linkdin from '../../assets/images/free-icon-linkedin-220343.png'
 import upwork from '../../assets/images/upwork_logo_icon_170310.png'
 // import { SubtitlesOutlined } from '@mui/icons-material';
-const Contacts = ()=>{
+const Contacts = () => {
     return (
         <section className={styles.contacts}>
-        <Container>
-        
-            <h2 className={styles.title}> GET IN TOUCH</h2>
-            <h3 className={styles.text}> We are looking forward to start work with you.</h3>
+            <Container>
 
-            
-            <div className={styles.list}>
+                <h2 className={styles.title}> GET IN TOUCH</h2>
+                <h3 className={styles.text}> We are looking forward to start work with you.</h3>
+
+
+
                 <ul className={styles.list}>
                     <li className={styles.listItem}>
-                       <a className={styles.email}  href="tel:+44 207 175 5616"> Phone: +44 207 175 5616 </a> 
+                        <a className={styles.contact} href="tel:+44 207 175 5616"> Phone: +44 207 175 5616 </a>
                     </li>
                     <li className={styles.listItem}>
-                    <a className={styles.email} href="e-mail: contact@infopoly.com"> E-mail: contact@infopoly.com </a>  
-                    </li>
-                    <li className={styles.listItem}> 
-                        Adress: .....
-
+                        <a className={styles.contact} href="e-mail: contact@infopoly.com"> E-mail: contact@infopoly.com </a>
                     </li>
                     <li className={styles.listItem}>
-                    <a href="https://www.linkedin.com/company/infopoly-ltd/?original_referer=https%3A%2F%2Fwww.infopoly.com%2F"><img src={linkdin} className={styles.linkdin}/></a>
+                        Adress: 221B Baker Street, London
 
-                    </li>
-                    <li className={styles.listItem}>
-                    <a href='https://www.upwork.com/nx/wm/client/dashboard'><img src={upwork} className={styles.linkdin}/></a>
+                         </li>
+                    <div className={styles.socials}>
+                        <li className={styles.listItem}>
+                            <a href="https://www.linkedin.com/company/infopoly-ltd/?original_referer=https%3A%2F%2Fwww.infopoly.com%2F" className={styles.listItemA}><img src={linkdin} className={styles.listItemImg} /></a>
 
-                    </li>
-                    
+                        </li>
+                        <li className={styles.listItem}>
+                            <a href='https://www.upwork.com/nx/wm/client/dashboard' className={styles.listItemA}><img src={upwork} className={styles.listItemImg} /></a>
+                        </li>
+                    </div>
+
                 </ul>
-               
 
-            </div>
-           
-            {/* <img src={photo} className={styles.photo} ></img> */}
-           </Container>
+
+
+
+                {/* <img src={photo} className={styles.photo} /> */}
+            </Container>
         </section>
     )
 }
--- src/components/Ourservises/OurServices.js
+++ src/components/Ourservises/OurServices.js
@@ -3,27 +3,27 @@ import Container from "../Container/Container";
 import ServicesDta from './ServicesData';
 import ServiceCard from './ServiceCard/ServiceCard';
 
-const serviceData = ServicesDta ()
+const serviceData = ServicesDta()
 const OurServices = () => {
 
     return (
         <section className={styles.servicesWrapper}>
-        <Container>
-            <h1 className={styles.title}>Our IT services</h1>
-            <p className={styles.text}>
-              We provide to you the best choices. Adjust it to your needs and make sure
-              you get full lifecycle management from design, development, implementation and maintenance.
-              We can help and advise the most suitable technology for your requirements.
+            <Container>
+                <h1 className={styles.title}>Our IT services</h1>
+                <p className={styles.text}>
+                    We provide you the best choice. Adjust it to your needs and make sure
+                    you get full lifecycle management from design, development, implementation and maintenance.
+                    We can help and advise the most suitable technology for your requirements.
             </p>
-            <div className={styles.cardContainer}>
-                {serviceData.map(service => {
-                    return <ServiceCard key={service.id} service={service}/>
-                })
-                
-                }
-            </div>
-        </Container>
+                <div className={styles.cardContainer}>
+                    {serviceData.map(service => {
+                        return <ServiceCard key={service.id} service={service} />
+                    })
+
+                    }
+                </div>
+            </Container>
         </section>
-)
+    )
 }
 export default OurServices;
